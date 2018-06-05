270b33b069f6244ae15d24e6d9304ee1e12855a4
DakshaAuti <39920321+DakshaAuti@users.noreply.github.com>
2018-06-05 16:55:47 +0530

    Made changes for the test cases
    Add these files to project

---
+++ src/main/java/com/ubs/opsit/interviews/impl/BerlinClockObject.java
@@ -0,0 +1,41 @@
+package com.ubs.opsit.interviews.impl;
+
+public class BerlinClockObject {
+	private String firstRow;
+	private String secondRow;
+	private String thirdRow;
+	private String fourthRow;
+
+	public String getFirstRow() {
+		return firstRow;
+	}
+
+	public void setFirstRow(String firstRow) {
+		this.firstRow = firstRow;
+	}
+
+	public String getSecondRow() {
+		return secondRow;
+	}
+
+	public void setSecondRow(String secondRow) {
+		this.secondRow = secondRow;
+	}
+
+	public String getThirdRow() {
+		return thirdRow;
+	}
+
+	public void setThirdRow(String thirdRow) {
+		this.thirdRow = thirdRow;
+	}
+
+	public String getFourthRow() {
+		return fourthRow;
+	}
+
+	public void setFourthRow(String fourthRow) {
+		this.fourthRow = fourthRow;
+	}
+
+}
